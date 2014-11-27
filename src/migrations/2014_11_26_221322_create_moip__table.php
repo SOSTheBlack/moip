584f08f9ba11134509f0d3f2a75f418ed8cdb567
The Black <jeancesargarcia@gmail.com>
2014-11-27 11:29:32 -0200

    adicionado comentários para os meios de pagamentos em migrations

--- src/migrations/2014_11_26_221322_create_moip__table.php
+++ src/migrations/2014_11_26_221322_create_moip__table.php
@@ -15,24 +15,24 @@ class CreateMoipTable extends Migration {
 		Schema::create('moip', function($table){
 			$table->increments('id');
 			$table->string('receiver', 50)->comment('Identifica o usuário Moip que irá receber o pagamento no Moip');
-			$table->string('key', 40);
-			$table->string('token', 32);
-			$table->boolean('environment')->comment('Método que define o ambiente em qual o requisição será processada. False: produção, True: desenvolvimento');
-			$table->boolean('validate')->comment('False: Basic, True: Identification');
-			$table->string('reason')->comment(' Responsável por definir o motivo do pagamento');
-			$table->string('billet_expiration')->comment('Data em formato AAAA-MM-DD ou quantidade de dias');
-			$table->boolean('billet_working_days')->comment('Caso billet_expiration seja quantidade de dias você pode definir com true para que seja contado em dias úteis, o padrão será dias corridos');
+			$table->string('key', 40)->default('ABABABABABABABABABABABABABABABABABABABAB');
+			$table->string('token', 32)->default('01010101010101010101010101010101');
+			$table->boolean('environment')->default(0)->comment('Método que define o ambiente em qual o requisição será processada. False: desenvolvimento, True: produção');
+			$table->boolean('validate')->default(0)->comment('False: Basic, True: Identification');
+			$table->string('reason')->comment('Responsável por definir o motivo do pagamento');
+			$table->string('billet_expiration')->default(3)->comment('Data em formato AAAA-MM-DD ou quantidade de dias');
+			$table->boolean('billet_working_days')->default(0)->comment('Caso billet_expiration seja quantidade de dias você pode definir com true para que seja contado em dias úteis, o padrão será dias corridos');
 			$table->string('billet_firstLine')->comment('Mensagem adicionais a ser impresso no boleto');
 			$table->string('billet_secondLine')->comment('Mensagem adicionais a ser impresso no boleto');
 			$table->string('billet_lastLine')->comment('Mensagem adicionais a ser impresso no boleto');
 			$table->string('billet_urlLogo')->comment('URL de sua logomarca, dimenções máximas 75px largura por 40px altura');
 			$table->string('url_return')->comment('definir a URL que o comprador será redirecionado ao finalizar um pagamento através do checkout Moip');
 			$table->string('url_notification')->comment('responsável por definir a URL ao qual o Moip deverá notificar com o NASP');
-			$table->boolean('payment_creditCard');
-			$table->boolean('payment_billet');
-			$table->boolean('payment_financing');
-			$table->boolean('payment_debit');
-			$table->boolean('payment_debitCard');
+			$table->boolean('payment_billet')->comment('Para disponibilizar a opção "Boleto Bancário" como forma de pagamento no checkout Moip');
+			$table->boolean('payment_financing')->comment('Para disponibilizar a opção "Financiamento" como forma de pagamento no checkout Moip');
+			$table->boolean('payment_debit')->comment('Para disponibilizar a opção "Debito em conta" como forma de pagamento no checkout Moip');
+			$table->boolean('payment_credit_card')->comment('Para disponibilizar a opção "Cartão de Crédito" como forma de pagamento no checkout Moip');
+			$table->boolean('payment_debit_card')->comment('Para disponibilizar a opção "Cartão de débito" como forma de pagamento no checkout Moip');
 		});
 	}
 
